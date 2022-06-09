d053941a2b1b6851c1fa346ae50bfc10c4c503ce
MD Ayub N <mdayubn@yahoo.com>
2022-06-09 16:08:04 -0400

    My First Commit

--- Maven_Automation_Project/src/test/java/ReusableLibries/Reusable_Methods.java
+++
@@ -1,9 +0,0 @@
-package ReusableLibries;
-
-public class Reusable_Methods {
-    //creating a void methods to add two numbers
-    public static void addTwoNumber(int num1, int num2)
-    {
-        System.out.println("The result of two numbers is "+ (num1+num2));
-    }
-}
